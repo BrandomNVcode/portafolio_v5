188336de2e4234d7518b604e3dc6ad31c4c9123b
BrandomNV <noriegaverab@gmail.com>
2023-10-15 22:23:39 -0500

    .

--- src/components/navigation/NavigationSocials.js
+++ src/components/navigation/NavigationSocials.js
@@ -1,6 +1,6 @@
 import Linkedin from "../../assets/images/socials/linkedin-pink.svg";
 import Github from "../../assets/images/socials/github-pink.svg";
-import Codepen from "../../assets/images/socials/codepen-pink.svg";
+//import Codepen from "../../assets/images/socials/codepen-pink.svg";
 const NavigationSocials = () => {
   return (
     <section className="social-media">
